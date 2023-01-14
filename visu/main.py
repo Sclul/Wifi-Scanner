3bcf48d9e60d4c053acef2a02baac4cb70ab5664
Sclul <105013327+Sclul@users.noreply.github.com>
2023-01-14 22:33:39 +0100

    Implemeted dropdown menu for SSID-Selection

--- visu/main.py
+++ visu/main.py
@@ -1,44 +1,49 @@
+
 from dash import Dash, dcc, html, Input, Output
+import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
-import os
+
 
 app = Dash(__name__)
+
+data = pd.read_csv('/app/track_points.csv')
+
+
 app.layout = html.Div([
-    dcc.Graph(id='live-update-graph'),
-    dcc.Interval(
-        id='interval-component',
-        interval=10*1000, # in milliseconds
-        n_intervals=0
+
+        html.Div([
+            dcc.Dropdown(
+                data['ssid'].unique(),
+                'eduroam',
+                id='ssid-menu'
+            ),
+            dcc.RadioItems(
+                ['Linear', 'Log'],
+                'Linear',
+                id='xaxis-type',
+                inline=True
+            )
+        ], style={'width': '48%', 'display': 'inline-block'}),
+    dcc.Graph(
+        id='heatmap'
     )
 ])
 
-
-
-ssid = os.environ["SSID"]
-
-
-@app.callback(Output('live-update-graph', 'figure'),
-              Input('interval-component', 'n_intervals'))
-def update_graph_live(n):
+@app.callback(
+    Output('heatmap', 'figure'),
+    Input('ssid-menu', 'value'))
+def update_figure(selected_ssid):
     points = pd.read_csv('/app/track_points.csv')
+    filtered_points = points.drop(points[points.ssid != selected_ssid].index)
 
-    points = points.drop(points[points.ssid != ssid].index)
-
-    fig = go.Figure(go.Densitymapbox(lat=points.lat, lon=points.lon, z=points.strength,
-                                 radius=10))
-
+    fig = go.Figure(go.Densitymapbox(lat=filtered_points.lat, lon=filtered_points.lon, z=filtered_points.strength,radius=10))
     fig.update_geos(fitbounds="locations")
     fig.update_layout(mapbox_style="open-street-map", mapbox_center_lat=52.5156451, mapbox_center_lon=13.3256412, mapbox_zoom=16)
     fig.update_layout(height=1000 ,margin={"r":0,"t":0,"l":0,"b":0})
+
     return fig
 
 
-
-
-
-
-
-
-app.run_server(host='0.0.0.0',debug=True, port=8050)
-
+if __name__ == '__main__':
+    app.run_server(host='0.0.0.0',debug=True, port=8050)
